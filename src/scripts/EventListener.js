a1fa27dd1b5989224db0c72d698a4618ea377e96
Eren Dogan <eren.dogan@ozu.edu.tr>
2022-06-27 21:00:09 +0300

    Add normalize() to EventListener

--- src/scripts/EventListener.js
+++ src/scripts/EventListener.js
@@ -15,35 +15,49 @@ export class EventListener {
     }
 
     /**
+     * Normalize the key codes.
+     */
+    static normalize(key) {
+        switch(key) {
+            case KEY_W: return KEY_UP;
+            case KEY_A: return KEY_LEFT;
+            case KEY_S: return KEY_DOWN;
+            case KEY_D: return KEY_RIGHT;
+            default: return key;
+        }
+    }
+
+    /**
      * The function to be called when a key is down.
      */
     keyDown(event) {
+        const key = EventListener.normalize(event.which);
         if (loading == maxLoading) {
             if (Outrun.scene == MENU_SCENE) {
-                if (event.which == KEY_ENTER) {
+                if (key == KEY_ENTER) {
                     Outrun.scene = RADIO_SCENE;
                     AudioManager.play("coin");
                 }
             } else if (Outrun.scene == RADIO_SCENE) {
-                if (event.which == KEY_A | event.which == KEY_LEFT) {
+                if (key == KEY_LEFT) {
                     if (Radio.music != 0)
                         Radio.music--;
-                } else if (event.which == KEY_D | event.which == KEY_RIGHT) {
+                } else if (key == KEY_RIGHT) {
                     if (Radio.music != 2)
                         Radio.music++;
-                } else if (event.which == KEY_ENTER) {
+                } else if (key == KEY_ENTER) {
                     AudioManager.stop("wave");
                     Outrun.newGame();
                     Outrun.scene = IN_GAME_SCENE;
                 }
             } else if (Outrun.scene == IN_GAME_SCENE) {
-                if (event.which == KEY_W | event.which == KEY_UP) {
+                if (key == KEY_UP) {
                     Driver.accelerate = true;
-                } else if (event.which == KEY_S | event.which == KEY_DOWN) {
+                } else if (key == KEY_DOWN) {
                     Driver.decelerate = true;
-                } else if (event.which == KEY_A | event.which == KEY_LEFT) {
+                } else if (key == KEY_LEFT) {
                     Driver.steerLeft = true;
-                } else if (event.which == KEY_D | event.which == KEY_RIGHT) {
+                } else if (key == KEY_RIGHT) {
                     Driver.steerRight = true;
                 }
             }
@@ -54,14 +68,15 @@ export class EventListener {
      * The function to be called when a key is up.
      */
     keyUp(event) {
+        const key = EventListener.normalize(event.which);
         if (Outrun.scene == IN_GAME_SCENE) {
-            if (event.which == KEY_W | event.which == KEY_UP) {
+            if (key == KEY_UP) {
                 Driver.accelerate = false;
-            } else if (event.which == KEY_S | event.which == KEY_DOWN) {
+            } else if (key == KEY_DOWN) {
                 Driver.decelerate = false;
-            } else if (event.which == KEY_A | event.which == KEY_LEFT) {
+            } else if (key == KEY_LEFT) {
                 Driver.steerLeft = false;
-            } else if (event.which == KEY_D | event.which == KEY_RIGHT) {
+            } else if (key == KEY_RIGHT) {
                 Driver.steerRight = false;
             }
         }
@@ -69,13 +84,13 @@ export class EventListener {
 
 }
 
-let KEY_W = 87;
-let KEY_A = 65;
-let KEY_S = 83;
-let KEY_D = 68;
-let KEY_ENTER = 13;
-let KEY_SPACE = 32;
-let KEY_UP = 38;
-let KEY_LEFT = 37;
-let KEY_DOWN = 40;
-let KEY_RIGHT = 39;
+const KEY_W = 87;
+const KEY_A = 65;
+const KEY_S = 83;
+const KEY_D = 68;
+const KEY_ENTER = 13;
+const KEY_SPACE = 32;
+const KEY_UP = 38;
+const KEY_LEFT = 37;
+const KEY_DOWN = 40;
+const KEY_RIGHT = 39;
